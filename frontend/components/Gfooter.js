549c2a3b1cf6a8835711f34f6fcab8a33d3a1aaf
opti21 <40129778+opti21@users.noreply.github.com>
2021-04-18 23:21:13 -0500

    Funny footer change

--- frontend/components/Gfooter.js
+++ frontend/components/Gfooter.js
@@ -1,8 +1,10 @@
-import { Layout } from 'antd'
-const { Footer } = Layout
+import { Layout } from "antd";
+const { Footer } = Layout;
 
 export default function Gfooter() {
-    return (
-        <Footer style={{ textAlign: 'center' }}>Created with ❤ by <a href="https://github.com/opti21">opti21</a> ©2021</Footer>
-    );
+  return (
+    <Footer style={{ textAlign: "center" }}>
+      Not a collab <a href="https://github.com/opti21">opti21</a> ©2021
+    </Footer>
+  );
 }
--- frontend/components/Gheader.js
+++ frontend/components/Gheader.js
@@ -1,20 +1,29 @@
-import { Layout, Menu } from 'antd'
-import Title from 'antd/lib/skeleton/Title';
+import { Layout, Menu } from "antd";
+import Title from "antd/lib/skeleton/Title";
 const { Header } = Layout;
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Gheader(props) {
-    return (
-        <div>
-            <Header>
-                <div style={{ float: "left", width: "150px", height: "20px" }}>
-                    <p style={{ fontSize: "20px" }}>Gauntlet Bot</p>
-                </div>
-                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={[props.activePage]} >
-                    <Menu.Item key="1"><Link href="/current">Current</Link></Menu.Item>
-                    <Menu.Item key="2"><Link href="/previous-weeks">Previous Weeks</Link></Menu.Item>
-                </Menu>
-            </Header>
+  return (
+    <div>
+      <Header>
+        <div style={{ float: "left", width: "150px", height: "20px" }}>
+          <p style={{ fontSize: "20px" }}>Gauntlet Bot</p>
         </div>
-    );
+        <div style={{ float: "left", width: "150px", height: "20px" }}></div>
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={[props.activePage]}
+        >
+          <Menu.Item key="1">
+            <Link href="/current">Current</Link>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Link href="/previous-weeks">Previous Weeks</Link>
+          </Menu.Item>
+        </Menu>
+      </Header>
+    </div>
+  );
 }
