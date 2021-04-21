58b1d011d4f84cb01025d3ab779528f749e6d289
opti21 <40129778+opti21@users.noreply.github.com>
2021-04-21 00:53:01 -0500

    Removed session check for week api

--- frontend/pages/api/get-weeks.js
+++ frontend/pages/api/get-weeks.js
@@ -1,19 +1,13 @@
-require('dotenv').config()
-import { connectToDatabase } from "../../util/mongodb_backend"
-import { getSession } from 'next-auth/client'
+require("dotenv").config();
+import { connectToDatabase } from "../../util/mongodb_backend";
 
 export default async (req, res) => {
-    const session = await getSession({ req })
-    if (session) {
-        const { db } = await connectToDatabase();
-        const weeks = await db
-            .collection("gauntlet weeks")
-            .find({}, { "_id": 0 })
-            .sort({ "week": -1 })
-            .toArray()
+  const { db } = await connectToDatabase();
+  const weeks = await db
+    .collection("gauntlet weeks")
+    .find({}, { _id: 0 })
+    .sort({ week: -1 })
+    .toArray();
 
-        res.status(200).json(weeks)
-    } else {
-        res.status(401).json({ error: "Unathorized" })
-    }
-}
+  res.status(200).json(weeks);
+};
