07471070285af3ee7f5af2a371c6cd5a590ce038
opti21 <40129778+opti21@users.noreply.github.com>
2021-05-08 04:09:42 -0500

    added global declares for DBs

--- frontend/util/mongodb_backend.ts
+++ frontend/util/mongodb_backend.ts
@@ -1,17 +1,18 @@
-import { MongoClient } from 'mongodb'
+// @ts-ignore
+import { MongoClient } from "mongodb";
 
-const { MONGODB_URI, MONGODB_DB } = process.env
+const { MONGODB_URI, MONGODB_DB } = process.env;
 
 if (!MONGODB_URI) {
-    throw new Error(
-        'Please define the MONGODB_URI environment variable inside .env.local'
-    )
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local"
+  );
 }
 
 if (!MONGODB_DB) {
-    throw new Error(
-        'Please define the MONGODB_DB environment variable inside .env.local'
-    )
+  throw new Error(
+    "Please define the MONGODB_DB environment variable inside .env.local"
+  );
 }
 
 /**
@@ -19,30 +20,38 @@ if (!MONGODB_DB) {
  * in development. This prevents connections growing exponentially
  * during API Route usage.
  */
-let cached = global.mongo
+let cached = global.mongo;
 
 if (!cached) {
-    cached = global.mongo = { conn: null, promise: null }
+  cached = global.mongo = { conn: null, promise: null };
+}
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      mongo: any;
+    }
+  }
 }
 
 export async function connectToDatabase() {
-    if (cached.conn) {
-        return cached.conn
-    }
+  if (cached.conn) {
+    return cached.conn;
+  }
 
-    if (!cached.promise) {
-        const opts = {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        }
+  if (!cached.promise) {
+    const opts = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    };
 
-        cached.promise = MongoClient.connect(MONGODB_URI, opts).then((client) => {
-            return {
-                client,
-                db: client.db(MONGODB_DB),
-            }
-        })
-    }
-    cached.conn = await cached.promise
-    return cached.conn
+    cached.promise = MongoClient.connect(MONGODB_URI, opts).then((client) => {
+      return {
+        client,
+        db: client.db(MONGODB_DB),
+      };
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
--- frontend/util/prisma.ts
+++ frontend/util/prisma.ts
@@ -4,4 +4,12 @@ const prisma = global.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV === "development") global.prisma = prisma;
 
+declare global {
+  namespace NodeJS {
+    interface Global {
+      prisma: any;
+    }
+  }
+}
+
 export default prisma;
